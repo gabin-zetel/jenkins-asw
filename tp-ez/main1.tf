8528b3358bd3ea9bb933a8a5cacfbb01727fb972
gabin-zetel <gabin@zetelecom.fr>
2026-06-11 11:09:28 +0200

    Add TLS and local providers

--- tp-ez/main1.tf
+++ tp-ez/main1.tf
@@ -4,13 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
 }
-
-provider "aws" {
-  region = var.region
-}
-
 # Data source 
 data "aws_availability_zones" "available" {
   state = "available"
